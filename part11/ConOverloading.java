cf386cf7487e6ac3b5031edfec5c38b7653817e8
merry9504 <merry9504@naver.com>
2022-10-04 23:06:07 +0900

    java 11장 공부

---
+++ part11/ConOverloading.java
@@ -0,0 +1,38 @@
+package part11;
+
+class Person{
+    private  int regiNum;
+    private  int passNum;
+
+    Person(int rnum, int pnum){
+        regiNum = rnum;
+        passNum = pnum;
+    }
+
+     Person(int rnum){
+        regiNum = rnum;
+        passNum = 0;
+     }
+
+     void showPersonalInfo(){
+        System.out.println("주민등록 번호: " + regiNum);
+
+        if(passNum != 0)
+            System.out.println("여권 번호 : " + passNum + "\n");
+        else
+            System.out.println("여권을 가지고 있지 않습니다. \n");
+     }
+}
+
+public class ConOverloading {
+    public static void main(String[]args){
+        //여권이 있는 사람의 정보를 담은 인스턴스 생성
+        Person jung = new Person(335577,112233);
+
+        //여권이 없는 사람의 정보를 담은 인스턴스 생성
+        Person hong = new Person(775544);
+
+        jung.showPersonalInfo();
+        hong.showPersonalInfo();
+    }
+}
---
+++ part11/ReturnStringBuilder.java
@@ -0,0 +1,24 @@
+package part11;
+
+public class ReturnStringBuilder {
+    public static void main(String[]args){
+        StringBuilder stb1 = new StringBuilder("123");
+        StringBuilder stb2 = stb1.append(45678);
+
+        System.out.println(stb1.toString());
+        System.out.println(stb2.toString());
+
+        //인덱스 0~4까지의 문자 삭제
+        stb2.delete(0,5);
+
+        System.out.println(stb1.toString());
+        System.out.println(stb2.toString());
+
+        //참조 값의 비교
+        if(stb1 == stb2){
+            System.out.println("같은 인스턴스 참조");
+        }else {
+            System.out.println("다른인스턴스 참조");
+        }
+    }
+}
