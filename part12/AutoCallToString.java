3a1d6c2ac5d685260989734ec2d58e04f2930010
merry9504 <merry9504@naver.com>
2022-10-05 22:50:51 +0900

    java 12장 공부

---
+++ part12/AutoCallToString.java
@@ -0,0 +1,26 @@
+package part12;
+
+class Box{
+    private String conts;
+
+    public Box(String conts) {
+        this.conts = conts;
+    }
+
+    public String toString(){
+        return conts;
+    }
+}
+
+public class AutoCallToString {
+    public static void main(String[]args){
+        StringBuilder stb = new StringBuilder("12");
+        stb.append(34);
+        System.out.println(stb.toString());
+        System.out.println(stb);
+
+        Box box = new Box("Camera");
+        System.out.println(box.toString());
+        System.out.println(box);
+    }
+}
