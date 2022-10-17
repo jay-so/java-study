49da5f6791c0b9f8321be444b426533249b5629f
jay-so <merry9504@naver.com>
2022-10-17 23:23:02 +0900

    java 6장 정리

--- part5/LabeledBreakPoint.java
+++ part5/LabeledBreakPoint.java
@@ -7,7 +7,7 @@ package part5;
 public class LabeledBreakPoint {
     public static void main(String[] args){
        outer: for(int i = 1; i<10;i++){//바깥쪽 for문에 레이블 outer명시
-        for(int j = 1; j<10;j++){ 
+        for(int j = 1; j<10;j++){
             if((i*j)==72){
                 System.out.println(i + " x "+ j + " = " + i*j);
                 break outer; //outer로 명시된 반복문까지 빠져나간다.
