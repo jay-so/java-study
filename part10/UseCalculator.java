3498788f97f7d37f089bae995e39fa771df6d18c
jay-so <merry9504@naver.com>
2022-10-25 17:09:24 +0900

    java 10장 정리

--- part10/UseCalculator.java
+++ part10/UseCalculator.java
@@ -25,6 +25,6 @@ public class UseCalculator {
         System.out.println("반지름 2.2, 원의 넓이: " + sc.calCircleArea(2.2)+ " \n");
 
         System.out.println("15 - 7 = " + sc.min(15,7));
-        System.out.println("반지름 5,0, 원의 넓이ㅖ: " + sc.calCirclePeri(5.0));
+        System.out.println("반지름 5,0, 원의 넓이: " + sc.calCirclePeri(5.0));
     }
 }
