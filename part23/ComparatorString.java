816fc04dfebd34bc7ee15d24c745cc0cb9974d55
jay-so <merry9504@naver.com>
2022-11-19 17:26:36 +0900

    java 23장 공부

---
+++ part23/ComparatorString.java
@@ -0,0 +1,24 @@
+package part23;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+class StringComparator implements Comparator<String>{
+    public int compare(String s1, String s2){
+        return s1.length() - s2.length();
+    }
+}
+
+public class ComparatorString {
+    public static void main(String[]args){
+        TreeSet<String> tree = new TreeSet<>(new StringComparator());
+        tree.add("Box");
+        tree.add("Rabbit");
+        tree.add("Robot");
+
+        for(String s: tree){
+            System.out.print(s.toString() + '\t');
+        }
+        System.out.println();
+    }
+}
---
+++ part23/HashSetEqualityTwo.java
@@ -0,0 +1,44 @@
+package part23;
+
+import java.util.HashSet;
+
+class Num2 {
+    private int num;
+    public Num2(int n) {
+        num = n;
+    }
+
+    @Override
+    public String toString(){
+        return String.valueOf(num);
+    }
+
+    @Override
+    public int hashCode(){
+        return num %3; //num의 값이 같으면 부류도 같다.
+    }
+
+    @Override
+    public boolean equals(Object obj){ //num의 값이 같으면 true qksghks
+        if(num == ((Num2)obj).num){
+            return  true;
+        }else{
+            return false;
+        }
+    }
+}
+
+public class HashSetEqualityTwo {
+    public static void main(String[]args){
+        HashSet<Num2> set = new HashSet<>();
+        set.add(new Num2(7799));
+        set.add(new Num2(9955));
+        set.add(new Num2(7799));
+        System.out.println("인스턴스 수: " + set.size());
+
+        for(Num2 n: set){
+            System.out.print(n.toString() + '\t');
+        }
+        System.out.println();
+    }
+}
