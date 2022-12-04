a5730b2a62b37b45459f22ebe1f04a1f777301c3
jay-so <merry9504@naver.com>
2022-12-04 18:19:31 +0900

    java 4장 정리

--- part4/CompAssignOp.java
+++ part4/CompAssignOp.java
@@ -1,12 +1,5 @@
 package part4;
-/*
-  복합 대입 연산자 = 대입 연산자가 다른 연산자와 묶여서 정의된 형태의 연산자
-  (일반): a = a+b  (대입) a +=b
-  (일반): a = a-b  (대입) a -=b
-  (일반): a = a*b  (대입) a *=b
-  (일반): a = a/b  (대입) a /=b
-  (일반): a = a%b  (대입) a %=b
- */
+
 public class CompAssignOp {
     public static void main(String[] args){
         short num = 10;
