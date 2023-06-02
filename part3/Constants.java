c4667dabae8f8beb314fae02d72e1e88b0d3bab1
jay-so <merry9504@naver.com>
2023-06-02 22:08:43 +0900

    Review java Syntax part3

--- part3/Constants.java
+++ part3/Constants.java
@@ -1,13 +1,14 @@
 package part3;
 
 public class Constants {
-    public static void main(String[] args){
-       final int TOTAL_SIZE = 100;
-       final char CONST_CHAR = '하';
-       final int CONST_ASSIGNED; //상수 선언
-        CONST_ASSIGNED = 12; //상수 CONST_ASSIGNED의 값은 12로 할당
-        System.out.println("상수1 : " +TOTAL_SIZE);
+
+    public static void main(String[] args) {
+        final int MAX_SIZE = 100;
+        final char CONST_CHAR = '상';
+        final int CONST_ASSIGED;
+        CONST_ASSIGED = 12; //할당하지 않았던 상수의 값 할당
+        System.out.println("상수1 : " + MAX_SIZE);
         System.out.println("상수2 : " + CONST_CHAR);
-        System.out.println("상수3 : " + CONST_ASSIGNED);
+        System.out.println("상수3 : " + CONST_ASSIGED);
     }
 }
--- part3/IntegerLiteral.java
+++ part3/IntegerLiteral.java
@@ -1,15 +1,17 @@
 package part3;
 
 public class IntegerLiteral {
-    public static void main(String[]args){
-        int binaryNumber = 0B100; //2진수 표현
-        int decimalNumber = 0130;  //8진수 표현
-        int octalNumber = 130;   //10진수 표현
-        int hexadecimal = 0x130;  //16진수 표현
+    public static void main(String[] args) {
+        int num1 = 123; //10진수 표현
+        int num2 = 0123; //8진수 표현
+        int num3 = 0x123; // 16진수 표현
 
-        System.out.println("2진수로 표현한 값 : " + binaryNumber);
-        System.out.println("8진수로 표현한 값 : " + decimalNumber);
-        System.out.println("10진수로 표현한 값 : " + octalNumber);
-        System.out.println("16진수로 표현한 값 : " + hexadecimal);
+        System.out.println("num1: " + num1);
+        System.out.println("num2: " + num2);
+        System.out.println("num3: " + num3);
+
+        System.out.println("11 + 22 + 33 = " + (11 + 22 + 33));
+        System.out.println("011 + 022 + 033 = " + (011 + 022 + 033));
+        System.out.println("0x11 + 0x22 + 0x33 = " + (0x11 + 0x22 + 0x33));
     }
 }
