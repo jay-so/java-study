154815593456a71be42eb82f275c4a52efde742f
jay-so <merry9504@naver.com>
2023-06-06 20:01:43 +0900

    review: review: java syntax part8

--- part8/com/wxfx/smart/Circle.java
+++ part8/com/wxfx/smart/Circle.java
@@ -1,21 +1,16 @@
 package part8.com.wxfx.smart;
-    /*
-    원의 넓이 관련 클래스 정의
-     */
+    //원의 널이 관련 클래스의 정의
 
 public class Circle {
     double rad;
     final double PI;
 
     public Circle(double r){
-        rad = r;
-        PI = 3.14;
+        this.rad = r;
+        this.PI = 3.14;
     }
-    /*
-    원의 넓이 반환
-     */
 
     public double getArea(){
-        return(rad*rad) *PI ;
+        return (rad * rad) * PI; //원의 넓이를 반환함
     }
 }
