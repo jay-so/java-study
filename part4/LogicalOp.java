d61cb4c3bb08da13adb66a46a6b05ba3e8fa3a1a
jay-so <merry9504@naver.com>
2023-06-06 17:11:54 +0900

    review: review: java syntax part4

--- part4/LogicalOp.java
+++ part4/LogicalOp.java
@@ -9,15 +9,15 @@ public class LogicalOp {
         boolean result;
 
         //변수 num1에 저장된 값이 1과 100 사이의 수인가?
-        result = (1 < num1)&&(num1 < 100);
-        System.out.println("1초과 100미만인가? " + result);
+        result = (1 < num1) && (num1 < 100);
+        System.out.println("1초과 100 미만인가?" + result);
 
         //변수 num2에 저장된 값이 2 또는 3의 배수인가?
-        result = ((num2 % 2) == 0 || ((num2 % 3) == 0));
-        System.out.println("2 또는 3의 배수인가? " + result);
+        result = ((num2 % 2== 0) ||((num2 % 3) == 0));
+        System.out.println("2 또는 3의 배수인가?" + result);
 
         //변수 num1이 0인가?
         result = !(num1 != 0);
-        System.out.println("0인가? " + result);
+        System.out.println("0인가?" + result);
     }
 }
