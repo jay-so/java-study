bd5b71918c7fef48e3899a74f0ee923e7b6b120d
jay-so <merry9504@naver.com>
2023-06-06 16:01:36 +0900

    review: review: java syntax part2

--- part2/OperationPromotion.java
+++ part2/OperationPromotion.java
@@ -1,10 +1,10 @@
 package part2;
 
 public class OperationPromotion {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         short num1 = 11;
         short num2 = 22;
-        int result = num1 + num2;
+        int result = num1 +num2;
         System.out.println(result);
     }
 }
--- part2/VariableDecl.java
+++ part2/VariableDecl.java
@@ -2,10 +2,10 @@ package part2;
 
 public class VariableDecl {
     public static void main(String[] args) {
-        double num1,num2; //두 개의 변수 동시 선언
+        double num1, num2; //두 개의 변수 동시에 선언
         double result;
-        num1 = 1.000001;
-        num2 = 2.000001;
+        num1 = 1.000000000001;
+        num2 = 2.000000000001;
         result = num1 + num2;
         System.out.println(result);
     }
